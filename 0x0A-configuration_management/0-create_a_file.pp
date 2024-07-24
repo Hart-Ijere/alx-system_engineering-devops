9f0a83a885a96632cfb92a81fb65efd3f74d48d0
Hart-Ijere <iyeowayi@gmail.com>
2024-07-24 04:43:49 +0100

    create a puppet manifest

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
-# Creates a file using Puppet
+# This Puppet manifest creates a file at /tmp/school with specific permissions, owner, group, and content.
 
 file { '/tmp/school':
-  ensure => 'file',
-  content => 'I love Puppet',
+  ensure  => 'file',
+  mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  mode    => '0744'
+  content => 'I love Puppet',
 }
