8ea29767b8c22cef76b469557dd25d4482cddd9d
Hart-Ijere <iyeowayi@gmail.com>
2024-08-18 23:24:41 +0100

    why apache is returning 500

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,22 @@
+# 0-strace_is_your_friend.pp
+# This Puppet manifest fixes the issue causing Apache to return a 500 error by ensuring the correct file permissions are set.
+
+exec { 'fix-apache-permissions':
+  command => 'chmod 755 /var/www/html',
+  path    => ['/bin', '/usr/bin'],
+  onlyif  => 'test $(stat -c "%a" /var/www/html) -ne 755',
+}
+
+file { '/var/www/html/index.php':
+  ensure => file,
+  mode   => '0644',
+  owner  => 'www-data',
+  group  => 'www-data',
+  source => 'puppet:///modules/your_module/index.php',
+}
+
+service { 'apache2':
+  ensure    => running,
+  enable    => true,
+  subscribe => Exec['fix-apache-permissions'],
+}
